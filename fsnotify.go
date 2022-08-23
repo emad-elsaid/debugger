8d1468e0ae36f8db96bd393c4b99e1a2d565b7cd
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2022-08-23 21:50:27 +0200

    Change arguments names

--- fsnotify.go
+++ fsnotify.go
@@ -9,17 +9,17 @@ import (
 )
 
 func WatchAddRecursive(watcher *fsnotify.Watcher, p string) error {
-	return filepath.WalkDir(p, func(parent string, d fs.DirEntry, err error) error {
+	return filepath.WalkDir(p, func(path string, info fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 
-		if strings.HasPrefix(d.Name(), ".") {
+		if strings.HasPrefix(info.Name(), ".") {
 			return fs.SkipDir
 		}
 
-		if d.IsDir() {
-			return watcher.Add(parent)
+		if info.IsDir() {
+			return watcher.Add(path)
 		}
 
 		return err
